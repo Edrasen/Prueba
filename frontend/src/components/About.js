6ad5cf1646fcbb937108c67f60ec668da81c4606
edrasen <alain42117@hotmail.com>
2021-03-30 07:38:23 -0600

    probando

---
+++ frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+
+export const About = () => (
+    <Fragment>
+        <h3> What is this about? </h3>
+        <p> In this project has been used different programming languajes and frameworks in both, backend and frontend developing the is a little bit more about that... </p>
+        <h3>Flask</h3>
+        <p>
+          Flask is a python minimalistic framework commonly used to build web apps, API's and small projects in general, currently there are too many third party libraries which can be used and implemented in your own project if they fit to it.
+
+        </p>
+    </Fragment>
+)
